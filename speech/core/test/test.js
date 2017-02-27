6f59381135b7b3f6dbde8df5b60c8b1843105c9f
sahibgoa <pandori@wisc.edu>
2017-02-27 14:31:27 -0600

    deleting speech files

--- speech/core/test/test.js
+++
@@ -1,8 +0,0 @@
-/** 
- * @fileoverview This is just a bunch of testing/demo nonsense. 
- * @author ehernandez4@wisc.edu (Evan Hernandez)
- */
-'use strict';
-var controller = new SpeechBlocks.Controller('blocklyDiv', 
-    {media: '../../external/blockly/media/',
-     toolbox: document.getElementById('toolbox')}); 
